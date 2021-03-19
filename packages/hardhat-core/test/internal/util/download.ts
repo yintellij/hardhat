02256c088748073170e156d480c8c51f9340eae7
Franco Victorio <victorio.franco@gmail.com>
2021-03-19 17:37:03 -0300

    Declare module for proxy package

--- packages/hardhat-core/test/internal/util/download.ts
+++ packages/hardhat-core/test/internal/util/download.ts
@@ -1,8 +1,6 @@
 import { assert } from "chai";
 import fsExtra from "fs-extra";
 import path from "path";
-// @ts-ignore
-// tslint:disable-next-line no-implicit-dependencies
 import Proxy from "proxy";
 
 import { download } from "../../../src/internal/util/download";
